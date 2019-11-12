3d8c8cb09ce6aee69361f808145716a98231a569
github username <github email address>
2019-11-12 15:55:09 -0700

    about page info with formatting

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,40 @@ import "../components-styles/About.css"
 export default function About () {
     return (
         <div className='about'>
-            <h2>About me!</h2>
+            <h2>My Experience</h2>
+            <ul>
+                <li>
+                    <h4>Software Developer</h4>
+                    <p>Full stack with experience in Ruby on Rails, React, CSS, and JavaScript.
+                    </p>
+                </li>
+                <li>
+                    <h4>Emergency Medical Technician (EMT)</h4>
+                    <h5>UCHealth North</h5>
+                    <p>Crisis management. Effective leadership, communication, and problem solving 
+                        in fast-paced, high-stress environments. Active 
+
+                    </p>
+                </li>
+                <li>
+                    <h4>Whitewater Raft Guide</h4>
+                    <p>Professionally guided on the Cache le Poudre River for 7 seasons.
+                        Large group leadership, organization, and communication.
+                    </p>
+                </li>
+            </ul>
+            <h2>Who I am</h2>
+            <ul>
+                <li>
+                    <h4>Lifelong Learner</h4>
+                    <p>Curious, knowledge seeking, and .</p>
+                </li>
+                <li>
+                    <h4>Outdoor Enthusiast</h4>
+                    <p> Fly fishing, Whitewater Kayaking & Rafting, Hiking, Rock-climbing, Camping and Mountain Biking.
+                    </p>
+                </li>
+            </ul>
         </div>
     )
 }
