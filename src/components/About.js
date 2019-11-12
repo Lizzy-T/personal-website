6d026c20d03a17ceb795d6ac0232cbddae753046
github username <github email address>
2019-11-12 13:33:39 -0700

    working nav bar. About, Home, and Resume pages styled. Color scheme established

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../components-styles/About.css"
+
+export default function About () {
+    return (
+        <div className='about'>
+            <h2>About me!</h2>
+        </div>
+    )
+}
